0ef6cb5b1a8a3f178a568dceaf2d823836eda0b6
Dakota Clark <bannaray95@gmail.com>
2019-01-27 02:19:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,14 +3,14 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Rowdy", 1, "Male", "Black", "Loud", null);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Tabitha", 12, "Female", "White", "Gentle", null);
+  VALUES ("Tabitha", 6, "Female", "White", "Gentle", null);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Seargeant Brown", 5, "Male", "Brown", "Dominate", null);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Melissa", 1, "Female", "Black", "Shy", null);
+  VALUES ("Melissa", 13, "Female", "Black", "Shy", null);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Grinch", 7, "Male", "White", "Mean", null);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Wendy", 5, "Female", "Black", "Crafty", null);
-  INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Wendy", 6, "Female", "Black", "Crafty", null);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES (null, 6, "Male", "Light Brown", "Crazy", null);
