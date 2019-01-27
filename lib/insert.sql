27c4193ad9135c741c878a13e2979001d514bcdf
Dakota Clark <bannaray95@gmail.com>
2019-01-27 02:20:53 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,23 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Mr. Chocolate", 4, "Male", "Chocolate" "Sweet" null);
+  
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Rowdy", 1, "Male", "Black", "Loud", null);
+  
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Tabitha", 6, "Female", "White", "Gentle", null);
+  
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Seargeant Brown", 5, "Male", "Brown", "Dominate", null);
+  
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Melissa", 13, "Female", "Black", "Shy", null);
+  
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Grinch", 7, "Male", "White", "Mean", null);
+  
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Wendy", 6, "Female", "Black", "Crafty", null);
+  
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES (null, 6, "Male", "Light Brown", "Crazy", null);
